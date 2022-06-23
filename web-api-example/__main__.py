621b15ccefdf68d980b99b2e5b273366b31a37fb
Dave Thompson <dthompson@maystreet.com>
2022-06-23 19:48:22 -0400

    - added EngineRoom API example

---
+++ web-api-example/__main__.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import maystreet_data
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/aapl-hourly-avg/{year}/{month}/{day}")
+def hourly_avg(year: int, month: int, day: int):
+    query = f"""
+    SELECT 
+        DATE_TRUNC('hour', TO_TIMESTAMP(ExchangeTimestamp / 1000000000)) AS hour_ts,
+        AVG(price) as avg_price
+    FROM 
+        "prod_lake"."p_mst_data_lake".mt_trade
+    WHERE 
+        y = '{year}'
+        AND m = '{str(month).zfill(2)}'
+        AND d = '{str(day).zfill(2)}'
+        AND product = 'AAPL'
+        AND f = 'bats_edga'
+    GROUP BY 1
+    ORDER BY 1
+    """
+
+    return list(maystreet_data.query(maystreet_data.DataSource.DATA_LAKE, query))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
